575d122c24f063d3207e82ebdda372b72b81d52e
Jörg Kantel <joerg@kantel.de>
2021-09-04 17:38:33 +0200

    Update drawbot_test.py

--- drawbot_test.py
+++ drawbot_test.py
@@ -1,6 +1,10 @@
+size(640, 480)
+
 my_text = "🐍 Hallo DrawBöttchen! 🐍"
 print(my_text)
 
 fill(0.9, 0, 0)
-font("American Typewriter", 66)
+font("American Typewriter", 38)
 text(my_text, (80, width()/2))
+
+saveImage("images/drawbottest.jpg")
